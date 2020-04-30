6915eaa78f2565c166c2053734a2028147236ee4
bguzman012 <46763361+bguzman012@users.noreply.github.com>
2020-04-29 20:54:23 -0500

    DAO

---
+++ src/ec/edu/ups/est/bguzmanc/dao/TelefonoDAO.java
@@ -0,0 +1,5 @@
+package ec.edu.ups.est.bguzmanc.dao;
+
+public interface TelefonoDAO extends GenericDAO<TelefonoDAO, Integer> {
+
+}
---
+++ src/ec/edu/ups/est/bguzmanc/dao/UsuarioDAO.java
@@ -0,0 +1,5 @@
+package ec.edu.ups.est.bguzmanc.dao;
+
+public interface UsuarioDAO extends GenericDAO<UsuarioDAO, String>{
+
+}
