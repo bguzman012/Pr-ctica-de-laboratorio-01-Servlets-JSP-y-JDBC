8698e41320345541c08d84f70eb5c05a6f972194
bguzman012 <46763361+bguzman012@users.noreply.github.com>
2020-04-30 15:20:23 -0500

    JDBCUsuarioDAO

--- src/ec/edu/ups/est/bguzmanc/mysql/jdbc/JDBCUsuarioDAO.java
+++ src/ec/edu/ups/est/bguzmanc/mysql/jdbc/JDBCUsuarioDAO.java
@@ -2,7 +2,7 @@ package ec.edu.ups.est.bguzmanc.mysql.jdbc;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -25,7 +25,7 @@ public class JDBCUsuarioDAO extends JDBCGenericDAO<Usuario, String> implements U
 				+ "CORREO VARCHAR(100) NOT NULL, CONTRASENIA VARCHAR(25) NOT NULL"
 				+ " PRIMARY KEY (CEDULA))");
 		DAOGuia.getGuia().getUsuarioDAO().createTable();
-		
+
 	}
 
 	@Override
@@ -39,36 +39,32 @@ public class JDBCUsuarioDAO extends JDBCGenericDAO<Usuario, String> implements U
 				DAOGuia.getGuia().getTelefonoDAO().create(telefono);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public Usuario read(String id) {
 		// TODO Auto-generated method stub
 		Usuario usuario = null;
-		ResultSet rs = conexionUno.query("SELECT * FROM Usuario WHERE contrasenia=" + id);
+		ResultSet rs = conexionUno.query("SELECT * FROM Usuario WHERE contrasenia= '" + id + "'");
 		try {
 			usuario = new Usuario(rs.getString("cedula"), rs.getString("nombre"), rs.getString("apellido"), rs.getString("correo"), 
 					rs.getString("contrasenia"));
-			
+
 		}catch(SQLException e){
 			System.out.println(">>>WARNING (JDBCUserDAO:read): " + e.getMessage());
-			
+
 		}if(usuario == null) {
 			return null;
 		}
 		Set<Telefono> telefonos = DAOGuia.getGuia().getTelefonoDAO().findByUsuario(usuario.getCedula());
+
 		if(telefonos != null) {
-			Set<Telefono> telefonosFinal = new HashSet<Telefono>();
-			
-			for (Telefono telefono : telefonos) {
-				telefonosFinal.add(telefono);
-			}
-			usuario.setTelefonos(telefonosFinal);
+			usuario.setTelefonos(telefonos);
 		}
 
 		return usuario;
-		
+
 	}
 
 	@Override
@@ -77,7 +73,7 @@ public class JDBCUsuarioDAO extends JDBCGenericDAO<Usuario, String> implements U
 		TelefonoDAO telefonoDAO = DAOGuia.getGuia().getTelefonoDAO();
 		Set<Telefono> telefonos = telefonoDAO.findByUsuario(usuario.getContrasenia());	
 		conexionUno.update("UPDATE Usuario SET nombre = '" + usuario.getNombre() + "', apellido = '" + usuario.getApellido() + "', correo = '" 
-		+ usuario.getCorreo() + "', contrasenia ='" + usuario.getContrasenia() + "' WHERE cedula =" + usuario.getCedula());
+				+ usuario.getCorreo() + "', contrasenia ='" + usuario.getContrasenia() + "' WHERE cedula = '" + usuario.getCedula() + "'");
 
 		if (usuario.getTelefonos() == null && telefonos != null) {
 			for (Telefono telefono : telefonos) {
@@ -87,29 +83,52 @@ public class JDBCUsuarioDAO extends JDBCGenericDAO<Usuario, String> implements U
 			for (Telefono telefono : usuario.getTelefonos()) {
 				telefonoDAO.create(telefono);
 			}
-		} else if(usuario.getTelefonos() !=null)	
-		} else if (shoppingBasket.getProducts() != null && products != null) {
-			for (Product product : shoppingBasket.getProducts()) {
-				productDAO.update(product);
+		} else if(usuario.getTelefonos() !=null && telefonos != null) {
+			for (Telefono telefono : usuario.getTelefonos()) {
+				telefonoDAO.update(telefono);
 			}
-		}*/
+		}
 
-		
-		
+
+
 	}
 
 	@Override
-	public void delete(Usuario entity) {
+	public void delete(Usuario usuario) {
 		// TODO Auto-generated method stub
-		
+		if (usuario.getTelefonos() != null) {
+			for (Telefono telefonos : usuario.getTelefonos()) {
+				DAOGuia.getGuia().getTelefonoDAO().delete(telefonos);
+			}
+		}
+		conexionUno.update("DELETE FROM Usuario WHERE contrasenia = '" + usuario.getContrasenia() + "'");
+
 	}
 
 	@Override
 	public List<Usuario> find() {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Usuario> list = new ArrayList<Usuario>();
+		ResultSet rs = conexionUno.query("SELECT * FROM Usuario");
+		try {
+			while (rs.next()) {
+				Usuario usuario = new Usuario(rs.getString("cedula"), rs.getString("nombre"), rs.getString("apellido"), rs.getString("correo"), 
+						rs.getString("contraenia"));
+
+				Set<Telefono> telefonos = DAOGuia.getGuia().getTelefonoDAO().
+						findByUsuario(usuario.getCedula());
+				if(telefonos != null) {
+					usuario.setTelefonos(telefonos);
+				}
+				list.add(usuario);
+			}
+
+		}catch (SQLException e) {
+				System.out.println(">>>WARNING (JDBCShoppingBasketDAO:find): " + e.getMessage());
+		}
+		
+		return list;
+		
+		} 
+
 	}
-
-	
-
-}
